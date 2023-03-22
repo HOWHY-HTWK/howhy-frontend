282acce07bba6f4762704f2f30bab325f63b97e0
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-03-22 16:24:16 +0100

    implement axios

---
+++ reactFrontend/axios-client.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+     
+})
+
+// axiosClient.interceptors.request.use( (config: )=> {
+
+// })
+
+export default axiosClient;
--- reactFrontend/src/App.jsx
+++ reactFrontend/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axiosClient from '../axios-client'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [videoData, setVideoData] = useState(null)
+
+  
+    axiosClient.get("videoDatas").then((response) => {
+      setVideoData(response.data);
+    });
+
 
   return (
+    <>
     <div id="videoframe">
-      <iframe id="iframe" src="https://mediaserver.htwk-leipzig.de/permalink/v12663c723847flqwp36/iframe/#start=30" frameborder="0" allowfullscreen
-        onLoad="iframeDidLoad();"></iframe>
+      <iframe id="iframe" src="https://mediaserver.htwk-leipzig.de/permalink/v12663c723847flqwp36/iframe/#start=30"></iframe>
       <div id="question_div"></div>
     </div>
+    <div>{videoData}</div>
+    </>
   )
 }
 
