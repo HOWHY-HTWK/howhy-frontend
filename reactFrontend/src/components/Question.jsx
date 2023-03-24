8a447c982cb0a2b1864380f4bc41d18f52adc9c4
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-03-24 15:36:22 +0100

    only feedback

--- reactFrontend/src/components/Question.jsx
+++ reactFrontend/src/components/Question.jsx
@@ -2,28 +2,28 @@ import React, { useState, useEffect } from 'react'
 import './css/questions.css'
 import axiosClient from '../../axios-client'
 
-export default function QuestionBox({questionData, setVideoData, videoId}) {
-  var selected = Array.from({length: questionData.answers.length},() => false);
+export default function QuestionBox({ questionData, setVideoData, videoId }) {
+  var selected = Array.from({ length: questionData.answers.length }, () => false);
 
   const [selectedAnswers, setselectedAnswers] = useState(selected);
   const [answerCorrect, setAnswerCorrect] = useState(null);
-  const answers = questionData.answers.map((answer, index) => <div key={index} className={`questionElements answer ${selectedAnswers[index] ? "whiteborder" : "" }`} onClick={() => selectAnswer(index)}>{answer}</div>);
-  
-  function selectAnswer(index){
+  const answers = questionData.answers.map((answer, index) => <div key={index} className={`questionElements answer ${selectedAnswers[index] ? "whiteborder" : ""}`} onClick={() => selectAnswer(index)}>{answer}</div>);
+
+  function selectAnswer(index) {
     var arr = selectedAnswers.slice(0);
     arr[index] = arr[index] == false ? true : false;
     setselectedAnswers(arr);
   }
 
-  function sendAnswer(){
+  function sendAnswer() {
     var answerIndexes = [];
     for (let step = 0; step < selectedAnswers.length; step++) {
-      if(selectedAnswers[step]){
+      if (selectedAnswers[step]) {
         answerIndexes.push(step);
       }
     }
     processAnswer(answerIndexes);
-    
+
   }
 
   function processAnswer(answer) {
@@ -32,39 +32,42 @@ export default function QuestionBox({questionData, setVideoData, videoId}) {
         "questionIndex": questionData.index,
         "answers": answer
       }
-      axiosClient.post(`videoDatas/checkAnswers/${videoId}`, request )
+      axiosClient.post(`videoDatas/checkAnswers/${videoId}`, request)
         .then((response) => {
           setAnswerCorrect(response.data.success)
-          setTimeout(function() {
+          setTimeout(function () {
             setAnswerCorrect(null)
             setVideoData(null)
           }, 2000);
-        
+
         })
     }
   }
 
-  function answerFeedback(){
-    if(answerCorrect != null){
+  function answerFeedback() {
+    if (answerCorrect != null) {
       return (
         <div>{answerCorrect ? "Richtig!" : "Leider Falsch"}</div>
-        )
+      )
+    } else {
+      return (
+        <div id="question">
+          <div className="questionElements" id="questionText"> {questionData.question}</div>
+          <div id="answersWrapper">
+            {answers}
+          </div>
+          <div id="answerbuttonWrapper">
+            <div className="questionElements" id="answerbutton" onClick={sendAnswer}>Antwort abschicken</div>
+          </div>
+        </div>
+      )
     }
   }
-  
+
   return (
     <>
-      <div id="question">
-        <div className="questionElements" id="questionText"> {questionData.question}</div>
-        <div id="answersWrapper">
-          {answers}
-        </div>
-        <div id="answerbuttonWrapper">
-          <div className="questionElements" id="answerbutton" onClick={sendAnswer}>Antwort abschicken</div>
-        </div>
-      </div>
       {answerFeedback()}
-      </>
+    </>
 
   )
 }
