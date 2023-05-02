905cdd7ba1470a54df18dfb46683dee268d02651
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-02 11:17:34 +0200

    use remember token

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -12,25 +12,27 @@ export default function LogIn({returnSuccess = (success) => success, showEditorO
 
   const emailRef = createRef()
   const passwordRef = createRef()
+  const rememberRef = createRef()
+  const [remember, setRemember] = useState(false)
 
   const [signUp, setSignUp] = useState(false);
 
   function handleLogin(e) {
-    e.preventDefault();
-    let formData = new FormData(e.target)
-    let email = emailRef.current.value;
-    let password = passwordRef.current.value;
+    e.preventDefault();    
     axiosClient.get('/sanctum/csrf-cookie')
       .then(response => {
+        debugger
         axiosClient.post('/login', {
-          email: email,
-          password: password
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+          remember: remember
         }).then(response => {
           console.log(response)
           setUser(response.data)
           returnSuccess(true)
         }).catch((error) => {
           alert(error.response.data.message)
+          console.log(error.response)
         });
       })
   }
@@ -52,8 +54,8 @@ export default function LogIn({returnSuccess = (success) => success, showEditorO
           <span>Log In</span>
           <label><input ref={emailRef} type="email" name="email" placeholder='E-mail' autoComplete='username' /></label>
           <label><input ref={passwordRef} type="password" name="password" placeholder='Passwort' autoComplete='current-password' /></label>
-          {/* <label>Eingeloggt Bleiben<input type='checkbox' name='remember'></input></label> */}
-          <button className={['button'].join(' ')}  type="submit">LogIn</button>
+          <label>Angemeldet Bleiben<input checked={remember} onChange={e => setRemember(e.target.checked)} type='checkbox' name='remember'></input></label>
+          <button  className={['button'].join(' ')}  type="submit">LogIn</button>
         </form>
         <div className={[styles.register].join(' ')}  onClick={toggleSignUp}> Registrieren</div>
       </div>
