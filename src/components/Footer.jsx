d99b04e818246275772304169e790e34720689b2
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-10 16:08:25 +0200

    bug report discord link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ import styles from './css/Footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.bar}>
-            <a href="mailto:alexander.bonin@htwk-leipzig.de?subject=Howhy Editor Fehler">
+            {/* <a href="mailto:alexander.bonin@htwk-leipzig.de?subject=Howhy Editor Fehler">
+                Fehler melden
+            </a> */}
+            <a href="https://discord.com/channels/1088437195532156938/1105857667920040017" target="_blank">
                 Fehler melden
             </a>
         </div>
