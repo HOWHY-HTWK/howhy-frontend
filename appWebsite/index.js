fa0c3a1eb5b85d0cd8cae27b0dea6dd267ea1dd3
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-15 11:21:17 +0200

    app page

--- appWebsite/index.js
+++ appWebsite/index.js
@@ -4,13 +4,13 @@ var input = document.getElementById("wrap");
 
 function showInput(){
     counter++
-    if(counter > 5){
+    if(counter > 4){
         input.style.visibility = 'visible';
     }
 }
 
 function navigate(){
     let code = document.getElementById("input").value
-    window.location.replace(`https://www.howhy.htwk-leipzig.de/${code}`);
+    window.location.replace(`https://howhy.htwk-leipzig.de/${code}`);
 
 }
