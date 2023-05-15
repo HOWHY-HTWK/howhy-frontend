4b183487063cc7c46251ca37383853fa5de8c7d9
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-15 18:37:20 +0200

    Revert "new user Login"
    This reverts commit 0118aee360e4c4025948e0fb2b443b222e122590.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import UserMenu from './components/UserMenu'
-import UserLogin from './components/UserLogin'
+import LogIn from './components/LogIn'
 import { useStateContext } from './contexts/ContextProvider'
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
         <Footer></Footer>
       </div>
       :
-      <UserLogin returnSuccess={success => setLoginActive(!success)} />}
+      <LogIn returnSuccess={success => setLoginActive(!success)}></LogIn>}
       <div className={[styles.toggle].join(' ')} onClick={toggleFrame}>Toggle Frame</div>
 
     </div>
