5d6a7a7085ae09a4f1764df0bc430707e26daaa1
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-22 16:26:25 +0200

    signup improved

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import axiosClient from '../../axios-client.jsx'
 import styles from './css/Login.module.css'
 import { useStateContext } from '../contexts/ContextProvider.jsx'
@@ -6,12 +6,27 @@ import { useStateContext } from '../contexts/ContextProvider.jsx'
 export default function SignUp({ toggleSignUp, showEditorOption = false }) {
   const { user, setUser } = useStateContext()
 
+  const [info, setInfo] = useState(false)
+  const infoRef = useRef()
+
   const [signUpData, setsignUpData] = useState({
     name: makeUserName(6),
     email: '', password: '',
     repeatPassword: '',
     editorRights: showEditorOption ? true : false
   })
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!infoRef.current.contains(event.target)) {
+        setInfo(false)
+      }
+    };
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   function handleSignup(e) {
     e.preventDefault();
@@ -63,53 +78,85 @@ export default function SignUp({ toggleSignUp, showEditorOption = false }) {
       })
   }
 
+  function getUserNameInfo() {
+    return (
+      <div className={[styles.infoBox, info ? styles.visible : null].join(' ')} >Es wird automatisch ein anonymer Benutzername generiert. Dieser kann allerdings wie gewünscht angepasst werden.</div>
+    )
+  }
+
+  function handleInfoClick() {
+    setInfo(prev => !prev)
+  }
+
   return (
     <div className={[styles.formwrapper].join(' ')} >
       <form method="post" className={[styles.vertical].join(' ')} onSubmit={handleSignup}>
         <span>Registrieren</span>
-        <input
-        className={[styles.input].join(' ')} 
-          value={signUpData.name}
-          onInput={e => setsignUpData({ ...signUpData, name: e.target.value })}
-          type="username"
-          name="username"
-          placeholder='Benutzername'
-          autoComplete='off' />
-        <input
-        className={[styles.input].join(' ')} 
-          value={signUpData.email}
-          onInput={e => setsignUpData({ ...signUpData, email: e.target.value })}
-          type="email"
-          name="email"
-          placeholder='E-mail'
-          autoComplete='email' />
-        <input
-        className={[styles.input].join(' ')} 
-          value={signUpData.password}
-          onInput={e => setsignUpData({ ...signUpData, password: e.target.value })}
-          type="password"
-          name="password"
-          placeholder='Passwort'
-          autoComplete='new-password' />
-        <input
-        className={[styles.input].join(' ')} 
-          value={signUpData.repeatPassword}
-          onInput={e => setsignUpData({ ...signUpData, repeatPassword: e.target.value })}
-          type="password"
-          name="repeat-password"
-          placeholder='Passwort wiederholen'
-          autoComplete='new-password' />
-        {signUpData.password != signUpData.repeatPassword ? <div className={[styles.error].join(' ')} >Keine übereinstimmung</div> : null}
-        {showEditorOption ?
-          <label className={[styles.requestRights].join(' ')} >
-            <input
-              checked={signUpData.editorRights}
-              onChange={() => setsignUpData({ ...signUpData, editorRights: !signUpData.editorRights })}
-              type='checkbox'></input>
-            Rechte zum Bearbeiten von Videos anfordern</label> : null}
+        <div className={[styles.label].join(' ')} >
+          <div>Benutzername
+            <span ref={infoRef} className={[styles.info].join(' ')} onClick={handleInfoClick}> &#9432; {getUserNameInfo()}</span>
+          </div>
+          <input
+            id="username"
+            className={[styles.input].join(' ')}
+            value={signUpData.name}
+            onInput={e => setsignUpData({ ...signUpData, name: e.target.value })}
+            type="username"
+            name="username"
+            placeholder='Benutzername'
+            autoComplete='off' />
+        </div>
+        <div className={[styles.label].join(' ')} >
+          <div className={[signUpData.email == '' ? styles.hidden : null].join(' ')} >E-Mail</div>
+          <input
+            className={[styles.input].join(' ')}
+            value={signUpData.email}
+            onInput={e => setsignUpData({ ...signUpData, email: e.target.value })}
+            type="email"
+            name="email"
+            placeholder='E-mail'
+            autoComplete='email' />
+        </div>
+        <div className={[styles.label].join(' ')} >
+          <div className={[signUpData.password == '' ? styles.hidden : null].join(' ')}>Passwort</div>
+          <input
+            className={[styles.input].join(' ')}
+            value={signUpData.password}
+            onInput={e => setsignUpData({ ...signUpData, password: e.target.value })}
+            type="password"
+            name="password"
+            placeholder='Passwort'
+            autoComplete='new-password' />
+        </div>
+        <div className={[styles.label].join(' ')} >
+          <div className={[signUpData.repeatPassword == '' ? styles.hidden : null].join(' ')} >Passwort wiederholen</div>
+          <input
+            className={[styles.input].join(' ')}
+            value={signUpData.repeatPassword}
+            onInput={e => setsignUpData({ ...signUpData, repeatPassword: e.target.value })}
+            type="password"
+            name="repeat-password"
+            placeholder='Passwort wiederholen'
+            autoComplete='new-password' />
+        </div>
+        {
+          signUpData.password != signUpData.repeatPassword ?
+            <div className={[styles.error].join(' ')} >Keine übereinstimmung</div>
+            : null
+        }
+        {
+          showEditorOption ?
+            <label className={[styles.requestRights].join(' ')} >
+              <input
+                checked={signUpData.editorRights}
+                onChange={() => setsignUpData({ ...signUpData, editorRights: !signUpData.editorRights })}
+                type='checkbox'></input>
+              Rechte zum Bearbeiten von Videos anfordern</label>
+            : null
+        }
         <button className={['button'].join(' ')} type="submit">Registrieren</button>
-      </form>
+      </form >
       <div className={[styles.register].join(' ')} onClick={toggleSignUp}>Einloggen</div>
-    </div>
+    </div >
   )
 }
