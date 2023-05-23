457c04e9669b7682a92e8dde7500f41a58abebc0
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-23 14:39:39 +0200

    verify email banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,62 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './App.module.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import UserMenu from './components/UserMenu'
 import LogIn from './components/LogIn'
+import { useStateContext } from './contexts/ContextProvider'
+import VerifyEmail from './pages/VerifyEmail'
+import VerifyEmailBanner from './components/VerifyEmailBanner'
+import axiosClient from '../axios-client'
 
 function App() {
-  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+	const windowSize = useRef([window.innerWidth, window.innerHeight]);
 
-  const [loginActive, setLoginActive] = useState(false)
-  const [frame, setFrame] = useState(determinIfSmartphone(windowSize.current));
-  
-  function toggleFrame() {
-    setFrame(prev => !prev)
-  }
+	const { user, setUser } = useStateContext()
 
-  function determinIfSmartphone(windowSize) {
-    if (windowSize[0] < 1000) {
-      return false
-    } else {
-      return true
-    }
-  }
+	const [loginActive, setLoginActive] = useState(false)
+	const [frame, setFrame] = useState(determinIfSmartphone(windowSize.current));
 
-  return (
-    <div className={[frame ? styles.phone : styles.phoneOff].join(' ')} >
-      {!loginActive ?
-      <div className={['pageWrap'].join(' ')} >
-        <Header><UserMenu setLoginActive={setLoginActive} /></Header>
-        <Outlet />
-        <Footer></Footer>
-      </div>
-      :
-      <LogIn returnSuccess={success => setLoginActive(!success)}></LogIn>}
-      <div className={[styles.toggle].join(' ')} onClick={toggleFrame}>Toggle Frame</div>
-    </div>
-  )
+	useEffect(() => {
+		refreshUser()
+	}, [])
+	
+	function refreshUser() {
+		axiosClient.get('/api/user').then(response => {
+			setUser(response.data)
+		})
+	}
+
+	function toggleFrame() {
+		setFrame(prev => !prev)
+	}
+
+	function determinIfSmartphone(windowSize) {
+		if (windowSize[0] < 1000) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+	return (
+		<div className={[frame ? styles.phone : styles.phoneOff].join(' ')} >
+			{!loginActive ?
+				<div className={['pageWrap'].join(' ')} >
+					<Header><UserMenu setLoginActive={setLoginActive} /></Header>
+					<Outlet />
+					<Footer></Footer>
+				</div>
+				: <LogIn returnSuccess={success => setLoginActive(!success)}></LogIn>
+			}
+			{user && user.email_verified_at == null ?
+				<VerifyEmailBanner></VerifyEmailBanner>
+				: null
+			}
+			<div className={[styles.toggle].join(' ')} onClick={toggleFrame}>Toggle Frame</div>
+		</div>
+	)
 }
 
 export default App
--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -8,6 +8,7 @@ import Footer from '../components/Footer'
 import DropDown from '../components/DropDown'
 import axios from 'axios'
 import styles from './css/Editor.module.css'
+import VerifyEmail from './VerifyEmail'
 
 export default function Editor() {
     const { user, setUser } = useStateContext()
@@ -43,12 +44,6 @@ export default function Editor() {
             )
     }, [])
 
-    function refreshUser() {
-        axiosClient.get('/api/user').then(response => {
-            setUser(response.data)
-        })
-    }
-
     function content() {
         return (
             <div className={['pageWrap'].join(' ')} >
@@ -61,27 +56,11 @@ export default function Editor() {
         )
     }
 
-    function verifiyEmail() {
-        return (
-            <div className={['center', styles.mail].join(' ')} >
-                Bitte verifizieren sie ihre E-mail adresse und drücken sie dann auf "Weiter"
-                <button className={['button'].join(' ')} onClick={resendLink}>Link erneut senden</button>
-                <button className={['button'].join(' ')} onClick={refreshUser}>Weiter</button>
-            </ div>
-        )
-    }
-
-    function resendLink() {
-        axiosClient.post('/email/verification-notification').then(response => {
-            alert('Ein neuer Link wurde gesendet')
-        })
-    }
-
     return user ?
         (user && user.role == 'creator' || user.role == 'admin') ?
             user.email_verified_at != null ?
                 content()
-                : verifiyEmail()
+                : <VerifyEmail></VerifyEmail>
             :
             (() => {
                 alert('Sie dürfen leider nicht auf diese seite zugreifen')
@@ -89,5 +68,4 @@ export default function Editor() {
             })()
         :
         <LogIn showEditorOption={true}></LogIn>
-
 }
