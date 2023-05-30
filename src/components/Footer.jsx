cbc7e99c6b8d742a2f730e30674f09feb48cfd62
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-05-30 15:39:37 +0200

    #80 done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from './css/Footer.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
         <div className={styles.bar}>
-            {/* <a href="mailto:alexander.bonin@htwk-leipzig.de?subject=Howhy Editor Fehler">
-                Fehler melden
-            </a> */}
-            <a href="https://discord.com/channels/1088437195532156938/1105857667920040017" target="_blank">
+            <a className={[styles.link].join(' ')} href="https://discord.com/channels/1088437195532156938/1105857667920040017" target="_blank">
                 Fehler melden
             </a>
+            <Link to={'/user/privacy'} className={[styles.link].join(' ')} >Datenschutz</Link>
+            <Link to={'/user/imprint'} className={[styles.link].join(' ')} >Impressum</Link>
         </div>
     )
 }
