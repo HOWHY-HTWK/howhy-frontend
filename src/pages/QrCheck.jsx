b5ad4da3f8f5363aa2408bc61b42c1a79655529a
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-06-09 15:36:46 +0200

    #104 scanner integrated

--- src/pages/QrCheck.jsx
+++ src/pages/QrCheck.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Html5QrcodePlugin from '../components/html5QrcodeScannerPlugin';
+import styles from './css/QrCheck.module.css'
 
 export default function QrCheck() {
     const code = useParams().code;
+
+    function onNewScanResult(decodedText, decodedResult) {
+        console.log(decodedText, decodedResult)
+    }
+
     return (
-        <div>{code}</div>
+        <div>
+            <div className={[styles.scanner].join(' ')} >
+                <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
+                    aspectRatio={1.0}
+                />
+            </div>
+        </div>
     )
 }
-
