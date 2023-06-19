c452c90a723ac20e81f96c81a6fea00975e71e7f
a-bonin <128031840+a-bonin@users.noreply.github.com>
2023-06-19 18:26:07 +0200

    frontend in subdirectory #171 done

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [svgr(), react()],
-  // base: '',
-  server: {
-    host: true,
-    port: 5173,
-  }
+    plugins: [svgr(), react()],
+    base: '/app',
+    server: {
+        host: true,
+        port: 5173,
+    }
 })
